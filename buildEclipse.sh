eb2f02b6eb98a842bbf6171c109b7b0d3bd86ec6
Kevin Harrington <mad.hephaestus@gmail.com>
2024-06-20 09:34:14 -0400

    Adding permissions set to the .app in Mac

--- buildEclipse.sh
+++ buildEclipse.sh
@@ -94,7 +94,7 @@ mkdir -p $SCRIPT_DIR/release
 NAME=Eclipse-Groovy
 case "${TYPE}" in
     Windows*)       MKPKG="7z a $SCRIPT_DIR/release/$NAME-$TYPE.zip $LOCATION/* ";;
-    Mac*)          MKPKG="cd $LOCATION/;zip -r $SCRIPT_DIR/release/$NAME-$TYPE.zip Eclipse.app; cd $SCRIPT_DIR";;
+    Mac*)          MKPKG="cd $LOCATION/; xattr -cr Eclipse.app; zip -r $SCRIPT_DIR/release/$NAME-$TYPE.zip Eclipse.app; cd $SCRIPT_DIR";;
     Linux*)         MKPKG="cd $DOWNDIR/$BASEFILE;tar czf $SCRIPT_DIR/release/$NAME-$TYPE.tar.gz * ;cd $SCRIPT_DIR";;
 esac
 echo "$MKPKG"
