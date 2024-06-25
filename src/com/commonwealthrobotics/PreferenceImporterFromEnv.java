760fe899e465ce0199007cf790c36d36020f663a
Kevin Bad Name <Kevin Bad Name@10.0.2.15>
2024-06-24 19:31:00 -0700

    update the preferences

--- src/com/commonwealthrobotics/PreferenceImporterFromEnv.java
+++ src/com/commonwealthrobotics/PreferenceImporterFromEnv.java
@@ -12,14 +12,22 @@ import org.eclipse.core.runtime.Platform;
 public class PreferenceImporterFromEnv implements IStartup {
 	@Override
 	public void earlyStartup() {
-		String epfPath = System.getenv("ECLIPSE_PREFERENCE_FILE");
+		String epfPath = System.getenv("ECLIPSE_PREFERENCE_FILE").strip();
 		if (epfPath != null && !epfPath.isEmpty()) {
+			if(epfPath.contains("\"")) {
+				epfPath = epfPath.replace("\"", "").strip();
+			}
 			File epfFile = new File(epfPath);
 			if (epfFile.exists()) {
+				System.out.println("EPF File Found at ECLIPSE_PREFERENCE_FILE "+epfPath);
 				PlatformUI.getWorkbench().getDisplay().asyncExec(() -> {
 					load(epfFile);
 				});
+			}else {
+				System.out.println("Preference file does not exist "+epfPath);
 			}
+		}else {
+			System.out.println("Preference file not provided via environment variable ECLIPSE_PREFERENCE_FILE");
 		}
 	}
 
@@ -27,6 +35,7 @@ public class PreferenceImporterFromEnv implements IStartup {
 		try {
 			IPreferencesService service = Platform.getPreferencesService();
 			FileInputStream inputStream = new FileInputStream(epfFile);
+			
 			service.importPreferences(inputStream);
 			inputStream.close();
 
