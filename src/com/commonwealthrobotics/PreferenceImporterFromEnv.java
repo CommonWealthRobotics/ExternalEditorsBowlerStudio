2f707b0d5f98da61ac3a97dacca92af7bb056f55
Kevin Bad Name <Kevin Bad Name@10.0.2.15>
2024-06-25 17:03:23 -0700

    Adding a check for already applied preferences and a prompt for restart when preferences are applied

--- src/com/commonwealthrobotics/PreferenceImporterFromEnv.java
+++ src/com/commonwealthrobotics/PreferenceImporterFromEnv.java
@@ -2,6 +2,9 @@ package com.commonwealthrobotics;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
@@ -12,14 +15,33 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-public class PreferenceImporterFromEnv extends AbstractUIPlugin implements IStartup {
-    public static final String PLUGIN_ID = "com.commonwealthrobotics.PreferenceImporterFromEnv";
-    @Override
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        log("PreferenceImporterFromEnv started");
-    }
+import org.osgi.service.prefs.Preferences;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.PlatformUI;
+
+public class PreferenceImporterFromEnv extends AbstractUIPlugin implements IStartup {
+	public static final String PLUGIN_ID = "com.commonwealthrobotics.PreferenceImporterFromEnv";
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		log("PreferenceImporterFromEnv started");
+	}
+
+	public static void promptForRestart() {
+		Display.getDefault().asyncExec(() -> {
+			Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+			boolean restart = MessageDialog.open(MessageDialog.QUESTION, shell, "Restart Required",
+					"Your one time workspace configurations have been applied. Do you want to restart now?", SWT.NONE);
+			if (restart) {
+				PlatformUI.getWorkbench().restart();
+			}
+		});
+	}
 	@Override
 	public void earlyStartup() {
 		String epfPath = System.getenv("ECLIPSE_PREFERENCE_FILE").strip();
@@ -44,16 +66,40 @@ public class PreferenceImporterFromEnv extends AbstractUIPlugin implements IStar
 	private void load(File epfFile) {
 		try {
 			IPreferencesService service = Platform.getPreferencesService();
-			FileInputStream inputStream = new FileInputStream(epfFile);
 			
-			service.importPreferences(inputStream);
-			inputStream.close();
-
-			// Force save of imported preferences
-			IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode("org.eclipse.ui.workbench");
-			prefs.flush();
-			String x = "Loaded Preferences from "+epfFile.getAbsolutePath();
-			log(x);
+			Preferences preferences = InstanceScope.INSTANCE.getNode("org.eclipse.jdt.core");
+			String compilerVersion = preferences.get("org.eclipse.jdt.core.compiler.compliance", "UNKNOWN");
+			String targetCompliance = "1.8";
+			try {
+				Scanner scanner = new Scanner(epfFile);
+				// now read the file line by line...
+				while (scanner.hasNextLine()) {
+					String line = scanner.nextLine();
+					if(line.contains("org.eclipse.jdt.core.compiler.compliance")) {
+						targetCompliance=line.split("=")[1];
+						scanner.close();
+						break;
+					}
+				}
+				scanner.close();
+			} catch (FileNotFoundException e) {
+				// handle this
+			}
+			if(!compilerVersion.contains(targetCompliance)) {
+				FileInputStream inputStream = new FileInputStream(epfFile);
+				
+				service.importPreferences(inputStream);
+				inputStream.close();
+	
+				// Force save of imported preferences
+				IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode("org.eclipse.ui.workbench");
+				prefs.flush();
+				String x = "Loaded Preferences from "+epfFile.getAbsolutePath();
+				log(x);
+				promptForRestart();
+			}else {
+				log("Preferences are already set, JVM version is "+compilerVersion);
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
